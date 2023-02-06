725806fa0a649b8fd424ce6555849a0652a1b0b2
Ana Solcan <solcan.anna@gmail.com>
2023-02-06 11:28:49 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+//  Require necessary NPM Packages
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+// Instantiate Express Application Object
+const app = express();
+
+// Define PORT for the API to run on
+const port = process.env.PORT || 5000;
+// REACT runs o port nr 3000 by default
+// so here we need to specifiy a different one
+// so they dont run on same port
+
+// Start the server and listen for requests on given port
+app.listen(port, () => console.log(`blogy is listening on port ${port}`));
