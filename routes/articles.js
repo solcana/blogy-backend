8c01ad55cca00b7ee7b1ae6fe099db5b3769d2b1
Ana Solcan <solcan.anna@gmail.com>
2023-02-06 16:03:24 +0100

    Add delete route

--- routes/articles.js
+++ routes/articles.js
@@ -60,6 +60,39 @@ router.get("/api/articles/:id", (req, res) => {
  * Description: ----- DELETE An Article by Article ID
  */
 
+router.delete("/api/articles/:id", (req, res) => {
+  Article.findById(req.params.id)
+    .then((article) => {
+      if (article) {
+        // step 1: find the article, if found
+        // Pass the result of Mongoose's 'remove' method
+        // to the next '.then'
+        // step 2: remove the article once found
+        return article.remove();
+        // this will remove article from database
+      } else {
+        // If we couldn't find a document with the matching ID
+        res.status(404).json({
+          error: {
+            name: "DocumentNotFoundError",
+            message: "The provided ID does not exist",
+          },
+        });
+      }
+    })
+    .then(() => {
+      // If the deletion succeded, return 204 and no JSON
+      res.status(204).end();
+    })
+    // This last part makes sure if there are any
+    // other problems, it will catch them
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    });
+});
+
+// use await and async, that do better then the above method but shorter
+
 /**
  * Action: ---- UPDATE
  * Method: ---- PUT/ PATCH
